ea1ef83b7919a41c4aef2b3c70f492ba7e6e1183
pjanus <pjanus paul.calinawan@truste.com>
2014-10-23 13:12:48 +0800

    Stub for Postgres Using the OpenID Connect Server

---
+++ openid-connect-postgres/src/main/resources/postgresql_scopes-1.0.1.sql
@@ -0,0 +1,3 @@
+INSERT INTO system_scope (scope, description, icon, allow_dyn_reg, default_scope, structured, structured_param_description) VALUES
+  ('company', 'company information', 'user', true, true, false, null);
+  
---
+++ openid-connect-postgres/src/main/resources/postgresql_scopes.sql
@@ -0,0 +1,8 @@
+INSERT INTO system_scope (scope, description, icon, allow_dyn_reg, default_scope, structured, structured_param_description) VALUES
+  ('openid', 'log in using your identity', 'user', true, true, false, null),
+  ('profile', 'basic profile information', 'list-alt', true, true, false, null),
+  ('email', 'email address', 'envelope', true, true, false, null),
+  ('address', 'physical address', 'home', true, true, false, null),
+  ('phone', 'telephone number', 'bell', true, true, false, null),
+  ('offline_access', 'offline access', 'time', true, true, false, null);
+  
